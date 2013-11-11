05426717940ebc20433eb0cf30d65f52a8c4f476
Damien Dolan <damien.dolan@dbiservices.com>
2013-11-11 09:03:24 -0500

    Added Enetity objects for Inventory as well as code for Inventory

---
+++ DBI.Data/DataFactory/Inventory/Inventory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBI.Data
+{
+     public partial class INVENTORY_V
+    {
+
+         public static List<INVENTORY_V> Inventory()
+         {
+             using (Entities _context = new Entities())
+             {
+                 return _context.Set<INVENTORY_V>().ToList();
+             }
+         
+         
+         }
+
+    }
+
+
+
+
+
+}
+
+
