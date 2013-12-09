68d9f2be6493023ef1c8327896ee098c389de121
Gene Lapointe <gene.lapointe@dbiservices.com>
2013-12-09 15:15:07 -0500

    EV-54 Updated equipment for new CLASS_CODES_V .

--- DBI.Data/DataFactory/Equipment/WebEquipment.cs
+++ DBI.Data/DataFactory/Equipment/WebEquipment.cs
@@ -13,7 +13,7 @@ namespace DBI.Data
             using(Entities _context = new Entities())
             {
                 List<WEB_EQUIPMENT_V> returnList = new List<WEB_EQUIPMENT_V>();
-                var data = from p in _context.PROJECTS_V
+                var data = from p in _context.CLASS_CODES_V
                                                 where p.PROJECT_TYPE == "TRUCK & EQUIPMENT"
                                                 select p;
                 if (OrganizationId != 0)
@@ -21,12 +21,12 @@ namespace DBI.Data
                     data = data.Where(p => p.CARRYING_OUT_ORGANIZATION_ID == OrganizationId);
                 }
 
-                List<PROJECTS_V> projectList = data.ToList();
+                List<CLASS_CODES_V> projectList = data.ToList();
 
-                foreach (PROJECTS_V project in projectList)
+                foreach (CLASS_CODES_V project in projectList)
                 {
                     WEB_EQUIPMENT_V rItem = new WEB_EQUIPMENT_V();
-                    //rItem.CLASS_CODE = project.CLASS_CODE;
+                    rItem.CLASS_CODE = project.CLASS_CODE;
                     rItem.NAME = project.NAME;
                     rItem.ORGANIZATION_NAME = project.ORGANIZATION_NAME;
                     rItem.ORG_ID = Double.Parse(project.ORG_ID.ToString());
