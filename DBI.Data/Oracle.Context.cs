7255237c5df41d027aaab8fa6fad54e77e2efb0e
Damien Dolan <damien.dolan@dbiservices.com>
2013-12-09 16:05:19 -0500

    EV-55 created and added Daily Activity Status table

--- DBI.Data/Oracle.Context.cs
+++ DBI.Data/Oracle.Context.cs
@@ -45,5 +45,6 @@ namespace DBI.Data
         public DbSet<DAILY_ACTIVITY_FOOTER> DAILY_ACTIVITY_FOOTER { get; set; }
         public DbSet<PROJECTS_V> PROJECTS_V { get; set; }
         public DbSet<CLASS_CODES_V> CLASS_CODES_V { get; set; }
+        public DbSet<DAILY_ACTIVITY_STATUS> DAILY_ACTIVITY_STATUS { get; set; }
     }
 }
--- DBI.Data/Oracle.Designer.cs
+++ DBI.Data/Oracle.Designer.cs
@@ -1,4 +1,4 @@
-﻿// Default code generation is disabled for model 'C:\Users\ljankowski\Desktop\Gitbucket\EV\DBI.Data\Oracle.edmx'. 
+﻿// Default code generation is disabled for model 'C:\Users\damien.dolan\Documents\Repo\EMSV2\DBI.Data\Oracle.edmx'. 
 // To enable default code generation, change the value of the 'Code Generation Strategy' designer
 // property to an alternate value. This property is available in the Properties Window when the model is
 // open in the designer.
