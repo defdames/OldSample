3a0434484c017860b9a4d5758f23d6deaada59c0
Lenny Jankowski <lenny.jankowski@dbiservices.com>
2014-01-09 16:52:55 -0500

    Added Changes for Mobile Daily Activity Submit

---
+++ DBI.Mobile.EMS/Controllers/DARSubmitController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using DBI.Data;
+using Newtonsoft.Json;
+
+namespace DBI.Mobile.EMS.Controllers
+{
+    public class DARSubmitController : ApiController
+    {
+        [Authorize]
+        [HttpPost]
+        public void Post(HttpRequestMessage req)
+        {
+            var data = req.Content.ReadAsStringAsync().Result;
+            DBI.Data.DAILY_ACTIVITY_HEADER headerRecord = JsonConvert.DeserializeObject<DBI.Data.DAILY_ACTIVITY_HEADER>(data);
+            GenericData.Insert<DBI.Data.DAILY_ACTIVITY_HEADER>(headerRecord);
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\temp\\post.json");
+            file.Write(data);
+            file.Close();
+        }
+
+    }
+}
