904c269fe010fbde0b47614745a98865649891c4
Lenny Jankowski <lenny.jankowski@dbiservices.com>
2014-01-13 07:32:25 -0500

    Unit of measure was commented out for iEMS

--- DBI.Mobile.EMS/Controllers/DARSubmitController.cs
+++ DBI.Mobile.EMS/Controllers/DARSubmitController.cs
@@ -17,13 +17,21 @@ namespace DBI.Mobile.EMS.Controllers
         [HttpPost]
         public void Post(HttpRequestMessage req)
         {
-            var data = req.Content.ReadAsStringAsync().Result;
-            DBI.Data.DAILY_ACTIVITY_HEADER headerRecord = JsonConvert.DeserializeObject<DBI.Data.DAILY_ACTIVITY_HEADER>(data);
-            GenericData.Insert<DBI.Data.DAILY_ACTIVITY_HEADER>(headerRecord);
+            //var data = req.Content.ReadAsStringAsync().Result;
+            //System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\temp\\post.json");
+            //file.Write(data);
+            //file.Close();
 
-            System.IO.StreamWriter file = new System.IO.StreamWriter("c:\\temp\\post.json");
-            file.Write(data);
-            file.Close();
+            //StreamReader re = new StreamReader("c:\\temp\\post.json");
+           // JsonTextReader reader = new JsonTextReader(re);
+
+            //JsonSerializer se = new JsonSerializer();
+            //object parsedData = se.Deserialize(reader);
+
+           // DBI.Data.DAILY_ACTIVITY_HEADER headerRecord = JsonConvert.DeserializeObject<DBI.Data.DAILY_ACTIVITY_HEADER>(data);
+
+            //DBI.Data.DAILY_ACTIVITY_HEADER headerRecord = object["daily_activity_header"];
+            //GenericData.Insert<DBI.Data.DAILY_ACTIVITY_HEADER>(headerRecord);
         }
 
     }
--- DBI.Mobile.EMS/Controllers/UnitOfMeasureController.cs
+++ DBI.Mobile.EMS/Controllers/UnitOfMeasureController.cs
@@ -19,31 +19,31 @@ namespace DBI.Mobile.EMS.Controllers
             return _context.UNIT_OF_MEASURE_V.ToList();   
         }
 
-        //[Authorize]
-        //[HttpGet]
-        //public IEnumerable<UNIT_OF_MEASURE_V> GetByDate(string fromDate)
-        //{
-        //    Entities _context = new Entities();
-        //    List<UNIT_OF_MEASURE_V> returnList = new List<UNIT_OF_MEASURE_V>();
+        [Authorize]
+        [HttpGet]
+        public IEnumerable<UNIT_OF_MEASURE_V> GetByDate(string fromDate)
+        {
+            Entities _context = new Entities();
+            List<UNIT_OF_MEASURE_V> returnList = new List<UNIT_OF_MEASURE_V>();
 
-        //    DateTime checkDate;
-        //    if (DateTime.TryParse(fromDate, out checkDate))
-        //    {
-        //        List<UNIT_OF_MEASURE_V> pt = _context.UNIT_OF_MEASURE_V.Where(p => p.LAST_UPDATE_DATE >= checkDate).ToList();
-        //        foreach (UNIT_OF_MEASURE_V uom in pt)
-        //        {
-        //            UNIT_OF_MEASURE_V nUOM = new UNIT_OF_MEASURE_V();
-        //            nUOM.UOM_CODE = uom.UOM_CODE;
-        //            nUOM.UNIT_OF_MEASURE = uom.UNIT_OF_MEASURE;
-        //            nUOM.UOM_CLASS = uom.UOM_CLASS;
-        //            nUOM.BASE_UOM_FLAG = uom.BASE_UOM_FLAG;
-        //            nUOM.DISABLE_DATE = uom.DISABLE_DATE;
-        //            nUOM.LAST_UPDATE_DATE = uom.LAST_UPDATE_DATE;
-        //            nUOM.ACTIVE = uom.ACTIVE;
-        //            returnList.Add(nUOM);
-        //        }
-        //    }
-        //    return returnList;
-        //}
+            DateTime checkDate;
+            if (DateTime.TryParse(fromDate, out checkDate))
+            {
+               List<UNIT_OF_MEASURE_V> pt = _context.UNIT_OF_MEASURE_V.Where(p => p.LAST_UPDATE_DATE >= checkDate).ToList();
+                foreach (UNIT_OF_MEASURE_V uom in pt)
+                {
+                    UNIT_OF_MEASURE_V nUOM = new UNIT_OF_MEASURE_V();
+                    nUOM.UOM_CODE = uom.UOM_CODE;
+                    nUOM.UNIT_OF_MEASURE = uom.UNIT_OF_MEASURE;
+                    nUOM.UOM_CLASS = uom.UOM_CLASS;
+                    nUOM.BASE_UOM_FLAG = uom.BASE_UOM_FLAG;
+                    nUOM.DISABLE_DATE = uom.DISABLE_DATE;
+                    nUOM.LAST_UPDATE_DATE = uom.LAST_UPDATE_DATE;
+                    nUOM.ACTIVE = uom.ACTIVE;
+                    returnList.Add(nUOM);
+                }
+            }
+            return returnList;
+        }
     }
 }
