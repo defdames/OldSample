7d5d94377ef2877b08f522173b4124284be86392
Gene Lapointe <gene.lapointe@dbiservices.com>
2014-01-20 09:57:54 -0500

    EV-15 #time 20m add back per diem page.

---
+++ DBI.Web.EMS/Views/Modules/DailyActivity/umChoosePerDiem.aspx
@@ -0,0 +1,39 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="umChoosePerDiem.aspx.cs" Inherits="DBI.Web.EMS.Views.Modules.DailyActivity.umChoosePerDiem" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <ext:ResourceManager ID="ResourceManager1" runat="server" IsDynamic="False" />
+        <ext:FormPanel runat="server" ID="uxChoosePerDiemFormPanel" Layout="FormLayout">
+            <Items>
+                <ext:ComboBox runat="server" ID="uxChoosePerDiemHeaderId" DisplayField="LONG_NAME" ValueField="HEADER_ID" FieldLabel="Choose Project for Per Diem">
+                    <Store>
+                        <ext:Store runat="server" ID="uxChoosePerDiemHeaderIdStore">
+                            <Model>
+                                <ext:Model runat="server">
+                                    <Fields>
+                                        <ext:ModelField Name="HEADER_ID" />
+                                        <ext:ModelField Name="LONG_NAME" />
+                                    </Fields>
+                                </ext:Model>
+                            </Model>
+                        </ext:Store>
+                    </Store>
+                </ext:ComboBox>
+            </Items>
+            <Buttons>
+                <ext:Button runat="server" Id="uxChoosePerDiemSubmitButton" Text="Submit">
+                    <DirectEvents>
+                        <Click OnEvent="deUpdatePerDiem" />
+                    </DirectEvents>
+                </ext:Button>
+            </Buttons>
+        </ext:FormPanel>
+    </form>
+</body>
+</html>
---
+++ DBI.Web.EMS/Views/Modules/DailyActivity/umChoosePerDiem.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DBI.Core.Web;
+using DBI.Core.Security;
+using DBI.Data;
+using Ext.Net;
+
+namespace DBI.Web.EMS.Views.Modules.DailyActivity
+{
+    public partial class umChoosePerDiem : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            FillComboBox(Request.QueryString["HeaderList"]);
+        }
+
+        protected void FillComboBox(string HeaderList)
+        {
+            Dictionary<string, string>[] Headers = JSON.Deserialize<Dictionary<string, string>[]>(HeaderList);
+
+            List<HeaderDetails> HeaderComboStore = new List<HeaderDetails>();
+
+            foreach (Dictionary<string, string> Header in Headers)
+            {
+                long HeaderId = long.Parse(Header["HEADER_ID"]);
+                string LongName = Header["LONG_NAME"].ToString();
+
+                HeaderComboStore.Add(new HeaderDetails()
+                {
+                    HEADER_ID = HeaderId,
+                    LONG_NAME = LongName
+                });
+            }
+            uxChoosePerDiemHeaderIdStore.DataSource = HeaderComboStore;
+        }
+
+        protected void deUpdatePerDiem(object sender, DirectEventArgs e)
+        {
+            long HeaderId = long.Parse(uxChoosePerDiemHeaderId.SelectedItem.Value.ToString());
+            DAILY_ACTIVITY_EMPLOYEE RecordToUpdate;
+            using (Entities _context = new Entities())
+            {
+                 RecordToUpdate = (from d in _context.DAILY_ACTIVITY_EMPLOYEE
+                                                          where d.HEADER_ID == HeaderId
+                                                          select d).Single();
+                RecordToUpdate.PER_DIEM = "Y";
+
+            }
+
+            GenericData.Update<DAILY_ACTIVITY_EMPLOYEE>(RecordToUpdate);
+
+            X.Js.Call("parent.App['uxSubmitActivityWindow'].show()");
+
+        }
+    }
+
+    public class HeaderDetails
+    {
+        public long HEADER_ID { get; set; }
+        public string LONG_NAME { get; set; }
+        public long PERSON_ID { get; set; }
+    }
+}
