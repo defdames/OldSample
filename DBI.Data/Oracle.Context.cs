8b78b1b4685b32e07945496247859d51c45c2a4f
Damien Dolan <damien.dolan@dbiservices.com>
2014-01-21 10:51:04 -0500

    EV-87  Created and added Sub Division table

--- DBI.Data/Oracle.Context.cs
+++ DBI.Data/Oracle.Context.cs
@@ -49,5 +49,6 @@ namespace DBI.Data
         public DbSet<CROSSING> CROSSINGS { get; set; }
         public DbSet<CROSSING_DATA_ENTRY> CROSSING_DATA_ENTRY { get; set; }
         public DbSet<CROSSING_CONTACTS> CROSSING_CONTACTS { get; set; }
+        public DbSet<CROSSING_SUB_DIVISION> CROSSING_SUB_DIVISION { get; set; }
     }
 }
