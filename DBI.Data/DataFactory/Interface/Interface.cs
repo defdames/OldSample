d5f6bf748638b6c63056da30d71982d441312589
Lenny Jankowski <lenny.jankowski@dbiservices.com>
2014-01-30 11:07:32 -0500

    Testing Oracle Post

---
+++ DBI.Data/DataFactory/Interface/Interface.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBI.Data
+{
+    public class Interface
+    {
+        public static XXDBI_DAILY_ACTIVITY_HEADER saveHeader(XXDBI_DAILY_ACTIVITY_HEADER header)
+        {
+            GenericData.Insert<XXDBI_DAILY_ACTIVITY_HEADER>(header);
+            return header;
+        }
+    }
+}
+
+
+
+
---
+++ DBI.Web.EMS/Post.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DBI.Data;
+
+namespace DBI.Web.EMS
+{
+    public partial class Post : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Post to Oracle
+
+            Entities _context = new Entities();
+
+            int pHeaderID = 148; // passed in from method
+
+            var daHeader = _context.DAILY_ACTIVITY_HEADER.Where(s => s.HEADER_ID  == pHeaderID)
+                            .Join(_context.PROJECTS_V, h => h.PROJECT_ID, p => p.PROJECT_ID, (h, p) =>
+                                new {
+                                    HEADER_ID = h.HEADER_ID,
+                                    ACTIVITY_DATE = h.DA_DATE,
+                                    STATE = h.STATE,
+                                    PROJECT_NUMBER = p.SEGMENT1,
+                                    PROJECT_NAME = p.NAME,
+                                    ORG_ID = p.ORG_ID,
+                                    }).SingleOrDefault();
+
+
+            XXDBI_DAILY_ACTIVITY_HEADER header = new XXDBI_DAILY_ACTIVITY_HEADER();
+            header.STATE = daHeader.STATE;
+            header.COUNTY = "None";
+            header.ACTIVITY_DATE = (DateTime)daHeader.ACTIVITY_DATE;
+            header.ORG_ID = (Decimal)daHeader.ORG_ID;
+            header.PROJECT_NUMBER = daHeader.PROJECT_NUMBER;
+            header.PROJECT_NAME = daHeader.PROJECT_NAME;
+            GenericData.Insert<XXDBI_DAILY_ACTIVITY_HEADER>(header);
+
+
+        }
+    }
+}
