ba94131feb01f72a872b40b6fa568d4e966e3485
Lenny Jankowski <lenny.jankowski@dbiservices.com>
2014-02-03 15:20:39 -0500

    Added Project Role Controller

---
+++ DBI.Mobile.EMS/Controllers/ProjectRoleController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DBI.Data;
+
+namespace DBI.Mobile.EMS.Controllers
+{
+    public class ProjectRoleController : ApiController
+    {
+
+        /// <summary>
+        /// This returns a list of employee roles
+        /// </summary>
+        /// <returns></returns>
+        [Authorize]
+        [HttpGet]
+        public IEnumerable<PA_ROLES_V> Get()
+        {
+            Entities _context = new Entities();
+            List<PA_ROLES_V> pl = _context.PA_ROLES_V.ToList();
+            return pl;
+        }
+
+         [Authorize]
+        [HttpGet]
+        public IEnumerable<PA_ROLES_V> GetByDate(string fromDate)
+        {
+            Entities _context = new Entities();
+           
+            DateTime checkDate;
+            if (DateTime.TryParse(fromDate, out checkDate))
+            {
+                 List<PA_ROLES_V> pl = _context.PA_ROLES_V.Where(p =>p.LAST_UPDATE_DATE >= checkDate).ToList();     
+            }
+            return pl;
+        }
+    }
+
+
+    }
+}
