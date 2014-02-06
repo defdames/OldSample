03c75ef839c3d1ef31c02b4a7d2f9759c5e2c3ef
Lenny Jankowski <lenny.jankowski@dbiservices.com>
2014-02-06 15:33:49 -0500

    Updated Interface

--- DBI.Data/DataFactory/Interface/Interface.cs
+++ DBI.Data/DataFactory/Interface/Interface.cs
@@ -8,35 +8,133 @@ namespace DBI.Data
 {
     public class Interface
     {
-        public static int nextHeaderID()
+        public static long generateDailyActivityHeaderSequence()
         {
             using (Entities _context = new Entities())
             {
                 string sql = @"select XXDBI.XXDBI_DAILYACT_HEADERID_S.NEXTVAL from dual";
-                decimal query = _context.Database.SqlQuery<decimal>(sql).First();
-                return int.Parse(query.ToString());
+                long query = _context.Database.SqlQuery<long>(sql).First();
+                return query;
             }
         }
 
-        //public int GetNextLaborHeaderID()
+        public static long generateLaborHeaderSequence()
+        {
+            using (Entities _context = new Entities())
+            {
+                string sql = @"select XXDBI.XXDBI_LABOR_HEADERID_S.NEXTVAL from dual";
+                long query = _context.Database.SqlQuery<long>(sql).First();
+                return query;
+            }
+        }
+
+        public static long generateInventorySequence()
+        {
+            using (Entities _context = new Entities())
+            {
+                string sql = @"select XXDBI.XXDBI_MTLTRANSID_S.NEXTVAL from dual";
+                long query = _context.Database.SqlQuery<long>(sql).First();
+                return query;
+            }
+        }
+
+
+        public IQueryable dailyActivityRecordById(long dailyActivityHeaderId)
+        {
+            using (Entities _context = new Entities())
+            {
+                var query = from h in _context.DAILY_ACTIVITY_HEADER
+                            join p in _context.PROJECTS_V on h.PROJECT_ID equals p.PROJECT_ID
+                            join l in _context.PA_LOCATIONS_V on p.LOCATION_ID equals (long)l.LOCATION_ID
+                            join u in _context.SYS_USER_INFORMATION on h.CREATED_BY equals u.USER_NAME
+                            where h.HEADER_ID == dailyActivityHeaderId
+                            select new { h, p, l, u };
+                return query;
+            }
+        }
+
+
+
+        //public static void insertDailyActivityHeader(long dailyActivityHeaderId, out long headerId)
         //{
-        //    return ExecuteFunc(() =>
+        //    try
         //    {
-        //        string sql = @"select XXDBI.XXDBI_LABOR_HEADERID_S.NEXTVAL from dual";
-        //        decimal query = ObjectContext.ExecuteStoreQuery<decimal>(sql).FirstOrDefault();
-        //        return int.Parse(query.ToString());
-        //    });
+        //        using (Entities _context = new Entities())
+        //        {
+        //            var query = from h in _context.DAILY_ACTIVITY_HEADER
+        //                        join p in _context.PROJECTS_V on h.PROJECT_ID equals p.PROJECT_ID
+        //                        join l in _context.PA_LOCATIONS_V on p.LOCATION_ID equals (long)l.LOCATION_ID
+        //                        join u in _context.SYS_USER_INFORMATION on h.CREATED_BY equals u.USER_NAME
+        //                        where h.HEADER_ID == dailyActivityHeaderId
+        //                        select new { h, p, l, u };
+
+        //            var data = query.Single();
+
+        //            XXDBI_DAILY_ACTIVITY_HEADER header = new XXDBI_DAILY_ACTIVITY_HEADER();
+        //            headerId = generateDailyActivityHeaderSequence();
+        //            header.DA_HEADER_ID = headerId;
+        //            header.STATE = data.l.REGION;
+        //            header.COUNTY = "NONE";
+        //            header.ACTIVITY_DATE = (DateTime)data.h.DA_DATE;
+        //            header.ORG_ID = (Decimal)data.p.ORG_ID;
+        //        header.PROJECT_NUMBER = data.p.SEGMENT1;
+        //        header.PROJECT_NAME = data.p.NAME;
+        //        header.CREATED_BY = data.u.USER_ID;
+        //        header.CREATION_DATE = DateTime.Now;
+        //        header.LAST_UPDATED_BY = data.u.USER_ID;
+        //        header.LAST_UPDATE_DATE = DateTime.Now;
+
+        //        var columns = new[] { "STATE", "COUNTY", "ACTIVITY_DATE", "ORG_ID", "PROJECT_NUMBER", "PROJECT_NAME", "CREATED_BY", "CREATION_DATE", "LAST_UPDATED_BY", "LAST_UPDATE_DATE" };
+        //        GenericData.Insert<XXDBI_DAILY_ACTIVITY_HEADER>(header, columns, "XXDBI.XXDBI_DAILY_ACTIVITY_HEADER");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw(ex);
+        //    }
         //}
 
-        //public int GetNextTransId()
+        //public static void insertDailyActivityLaborRecords(long dailyActivityHeaderId, long daHeaderId)
         //{
-        //    return ExecuteFunc(() =>
+        //    try
         //    {
-        //        string sql = @"select XXDBI.XXDBI_MTLTRANSID_S.NEXTVAL from dual";
-        //        decimal query = ObjectContext.ExecuteStoreQuery<decimal>(sql).FirstOrDefault();
-        //        return int.Parse(query.ToString());
-        //    });
+        //        using (Entities _context = new Entities())
+        //        {
+        //            var query = from h in _context.DAILY_ACTIVITY_HEADER
+        //                        join p in _context.PROJECTS_V on h.PROJECT_ID equals p.PROJECT_ID
+        //                        join l in _context.PA_LOCATIONS_V on p.LOCATION_ID equals (long)l.LOCATION_ID
+        //                        join u in _context.SYS_USER_INFORMATION on h.CREATED_BY equals u.USER_NAME
+        //                        where h.HEADER_ID == dailyActivityHeaderId
+        //                        select new { h, p, l, u };
+
+        //            var data = query.Single();
+
+        //            List<DAILY_ACTIVITY_EMPLOYEE> employees = data.h.DAILY_ACTIVITY_EMPLOYEE.ToList();
+                    
+        //            DAILY_ACTIVITY_PRODUCTION production = data.h.DAILY_ACTIVITY_PRODUCTION.FirstOrDefault();
+
+        //            foreach (DAILY_ACTIVITY_EMPLOYEE employee in employees)
+        //            {
+        //                XXDBI_LABOR_HEADER header = new XXDBI_LABOR_HEADER();
+        //                header.LABOR_HEADER_ID = generateLaborHeaderSequence();
+        //                header.DA_HEADER_ID = daHeaderId;
+        //                header.PROJECT_NUMBER = data.p.SEGMENT1;
+        //                header.TASK_NUMBER = production
+
+        //            }
+
+     
+
+        //            //var columns = new[] { "STATE", "COUNTY", "ACTIVITY_DATE", "ORG_ID", "PROJECT_NUMBER", "PROJECT_NAME", "CREATED_BY", "CREATION_DATE", "LAST_UPDATED_BY", "LAST_UPDATE_DATE" };
+        //            //GenericData.Insert<XXDBI_DAILY_ACTIVITY_HEADER>(header, columns, "XXDBI.XXDBI_DAILY_ACTIVITY_HEADER");
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw (ex);
+        //    }
         //}
+
 
 
         //public int GetNextPaTransID()
--- DBI.Web.EMS/Post.aspx.cs
+++ DBI.Web.EMS/Post.aspx.cs
@@ -14,44 +14,13 @@ namespace DBI.Web.EMS
         protected void Page_Load(object sender, EventArgs e)
         {
             // Post to Oracle
+             using (Entities _context = new Entities())
+                {
+                    DAILY_ACTIVITY_HEADER header = _context.DAILY_ACTIVITY_HEADER.Where(h => h.HEADER_ID == 61).SingleOrDefault();
+                    List<DAILY_ACTIVITY_EMPLOYEE.LABOR_HEADER_RECORD> employees = DAILY_ACTIVITY_EMPLOYEE.interfaceRecords(header.HEADER_ID, (DateTime)header.DA_DATE);
 
-
-            int pHeaderID = 148; // passed in from method
-
-            using (Entities _context = new Entities())
-            {
-                var query = from h in _context.DAILY_ACTIVITY_HEADER
-                                join p in _context.PROJECTS_V on h.PROJECT_ID equals p.PROJECT_ID
-                                join l in _context.PA_LOCATIONS_V on p.LOCATION_ID equals (long)l.LOCATION_ID
-                                join u in _context.SYS_USER_INFORMATION on h.CREATED_BY equals u.USER_NAME
-                                where h.HEADER_ID == pHeaderID
-                                select new {h, p, l, u};
-
-                var data  = query.Single();
-
-                Decimal generatedHeaderID = DBI.Data.Interface.nextHeaderID();
-
-                XXDBI_DAILY_ACTIVITY_HEADER header = new XXDBI_DAILY_ACTIVITY_HEADER();
-                header.STATE = data.l.REGION;
-                header.COUNTY = "NONE";
-                header.ACTIVITY_DATE = (DateTime)data.h.DA_DATE;
-                header.ORG_ID = (Decimal)data.p.ORG_ID;
-                header.PROJECT_NUMBER = data.p.SEGMENT1;
-                header.PROJECT_NAME = data.p.NAME;
-                header.CREATED_BY = data.u.USER_ID;
-                header.CREATION_DATE = DateTime.Now;
-                header.LAST_UPDATED_BY = data.u.USER_ID;
-                header.LAST_UPDATE_DATE = DateTime.Now;
-
-                var included = new[] { "STATE", "COUNTY", "ACTIVITY_DATE" };
-
-                GenericData.Insert<XXDBI_DAILY_ACTIVITY_HEADER>(header, included, "XXDBI.XXDBI_DAILY_ACTIVITY_HEADER");
-
-
-                
-  
-            }
-
+             }
         }
+
     }
 }
