0fbad4aa803cd32bcc057587f7b2b6b20dec69cf
Gene Lapointe <gene.lapointe@dbiservices.com>
2014-02-20 12:39:48 -0500

    EV-137 #time 1h Create ManageOrgs page markup.

--- DBI.Web.EMS/Views/Modules/CustomerSurveys/umManageOrgs.aspx.cs
+++ DBI.Web.EMS/Views/Modules/CustomerSurveys/umManageOrgs.aspx.cs
@@ -5,9 +5,14 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using DBI.Core.Security;
+using DBI.Core.Web;
+using DBI.Data;
+using Ext.Net;
+
 namespace DBI.Web.EMS.Views.Modules.CustomerSurveys
 {
-    public partial class umManageOrgs : System.Web.UI.Page
+    public partial class umManageOrgs : BasePage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
