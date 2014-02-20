600fd47560b2d7799759ac61154e430bf8a5c465
Gene Lapointe <gene.lapointe@dbiservices.com>
2014-02-20 10:36:18 -0500

    EV-137 #time 6h Create framework for admin area, with required pages, as well as creating the ManageForms markup.

---
+++ DBI.Web.EMS/Views/Modules/CustomerSurveys/umCustomerSurveyAdmin.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DBI.Core.Web;
+using DBI.Core.Security;
+using DBI.Data;
+using Ext.Net;
+
+namespace DBI.Web.EMS.Views.Modules.CustomerSurveys
+{
+    public partial class umCustomerSurveyAdmin : BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            uxAdminFormsTab.LoadContent();
+            
+        }
+    }
+}
