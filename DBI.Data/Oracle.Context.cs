4d6e631e8267e1b8cad25be1558c7e7990603359
Damien Dolan <damien.dolan@dbiservices.com>
2014-03-19 14:48:40 -0400

    EV-188 Created and added Org relationship table

--- DBI.Data/Oracle.Context.cs
+++ DBI.Data/Oracle.Context.cs
@@ -87,5 +87,6 @@ namespace DBI.Data
         public DbSet<XXDBI_PAYROLL_AUDIT> XXDBI_PAYROLL_AUDIT { get; set; }
         public DbSet<DAILY_ACTIVITY_EMPLOYEE> DAILY_ACTIVITY_EMPLOYEE { get; set; }
         public DbSet<ORG_HIER_V> ORG_HIER_V { get; set; }
+        public DbSet<SYS_USER_ORGS> SYS_USER_ORGS { get; set; }
     }
 }
