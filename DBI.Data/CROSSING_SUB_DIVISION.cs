a3cf577a19389c4c3e99309451f0497d687c3581
Damien Dolan <damien.dolan@dbiservices.com>
2014-05-27 16:47:51 -0400

    EV-285 Continual changes

--- DBI.Data/CROSSING_SUB_DIVISION.cs
+++ DBI.Data/CROSSING_SUB_DIVISION.cs
@@ -14,18 +14,14 @@ namespace DBI.Data
     
     public partial class CROSSING_SUB_DIVISION
     {
-        public CROSSING_SUB_DIVISION()
-        {
-            this.CROSSING_SERVICE_UNIT = new HashSet<CROSSING_SERVICE_UNIT>();
-        }
-    
         public long SUB_DIVISION_ID { get; set; }
         public string SUB_DIVISION_NAME { get; set; }
         public Nullable<System.DateTime> CREATE_DATE { get; set; }
         public Nullable<System.DateTime> MODIFY_DATE { get; set; }
         public string CREATED_BY { get; set; }
         public string MODIFIED_BY { get; set; }
+        public long SERVICE_UNIT_ID { get; set; }
     
-        public virtual ICollection<CROSSING_SERVICE_UNIT> CROSSING_SERVICE_UNIT { get; set; }
+        public virtual CROSSING_SERVICE_UNIT CROSSING_SERVICE_UNIT { get; set; }
     }
 }
