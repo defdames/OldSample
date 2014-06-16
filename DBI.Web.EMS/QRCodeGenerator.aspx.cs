76ebf528ca157c279ebd1daab1dd17db42263980
Lenny Jankowski <lenny.jankowski@dbiservices.com>
2014-06-16 09:27:57 -0400

    Added QRCodeGenerator test page

--- DBI.Web.EMS/QRCodeGenerator.aspx.cs
+++ DBI.Web.EMS/QRCodeGenerator.aspx.cs
@@ -23,34 +23,34 @@ namespace DBI.Web.EMS
             try
             {
                 BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
-                List<PROJECTS_V> equipment = new List<PROJECTS_V>();
+                List<string> equipment = new List<string>();
 
-                using(Entities _context = new Entities())
+                using (Entities _context = new Entities())
                 {
-                    equipment = _context.PROJECTS_V.Where(x => x.PROJECT_TYPE == "TRUCK & EQUIPMENT" && x.PROJECT_ID == 94).ToList();
+                    equipment = _context.PROJECTS_V.Where(x => x.PROJECT_TYPE == "TRUCK & EQUIPMENT").Select(x => x.NAME).Take(100).ToList();
                 }
 
-                    iTextSharp.text.Rectangle pgSize = new Rectangle(144f,72f);
-                    Document ExportedPDF = new Document(pgSize, 0f, 0f, 0f, 0f);
-                    PdfWriter ExportWriter = PdfWriter.GetInstance(ExportedPDF, new FileStream("c:\\temp\\truck.pdf",FileMode.Create));
-                    Paragraph NewLine = new Paragraph("\n");
-                    Font cellFont = FontFactory.GetFont("Verdana", 6);
-                    ExportedPDF.Open();
+                iTextSharp.text.Rectangle pgSize = new Rectangle(144f, 72f);
+                Document ExportedPDF = new Document(pgSize, 0f, 0f, 0f, 0f);
+                PdfWriter ExportWriter = PdfWriter.GetInstance(ExportedPDF, new FileStream("c:\\temp\\truck.pdf", FileMode.Create));
+                Font cellFont = FontFactory.GetFont("Verdana", 5);
+                ExportedPDF.Open();
 
-                    foreach (var equip in equipment)
-                    {
-                        System.Drawing.Image image = barcodeEncoder.Encode(BarcodeFormat.QRCode, equip.NAME);
-                        iTextSharp.text.Image QRCodeImage = iTextSharp.text.Image.GetInstance(image, System.Drawing.Imaging.ImageFormat.Png);
-                        QRCodeImage.Alignment = 1;
-                        ExportedPDF.Add(QRCodeImage);
+                foreach (var equip in equipment)
+                {
+                    System.Drawing.Image image = barcodeEncoder.Encode(BarcodeFormat.QRCode, equip);
+                    iTextSharp.text.Image QRCodeImage = iTextSharp.text.Image.GetInstance(image, System.Drawing.Imaging.ImageFormat.Jpeg);
+                    QRCodeImage.Alignment = 1;
+                    QRCodeImage.ScaleToFit(100f, 100f);
+                    ExportedPDF.Add(QRCodeImage);
 
-                        Paragraph Title = new Paragraph(equip.NAME, cellFont);
-                        Title.Alignment = 1;
-                        ExportedPDF.Add(Title);
-                        ExportedPDF.NewPage();
-                    }
+                    Paragraph Title = new Paragraph(equip, cellFont);
+                    Title.Alignment = 1;
+                    ExportedPDF.Add(Title);
+                    ExportedPDF.NewPage();
+                }
 
-                    ExportedPDF.Close();
+                ExportedPDF.Close();
             }
             catch (Exception ex)
             {
