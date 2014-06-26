a82c90836815a3a05fa72080130c71909bf0db58
Gene Lapointe <gene.lapointe@dbiservices.com>
2014-06-26 14:47:11 -0400

    EV-15 #time 15m Update to change cross business unit checks from errors to warning, and allow posting of those.  Also, only show projects that you have access to, no longer also displaying crossovers.

--- DBI.Web.EMS/Views/Modules/DailyActivity/umCombinedTab_DBI.aspx.cs
+++ DBI.Web.EMS/Views/Modules/DailyActivity/umCombinedTab_DBI.aspx.cs
@@ -111,11 +111,11 @@ namespace DBI.Web.EMS.Views.Modules.DailyActivity
                     if(Overlaps.Count > 0){
                         WarningList.AddRange(Overlaps);
                     }
+
                     WarningData EmployeeBusinessUnitFailures = ValidationChecks.EmployeeBusinessUnitCheck(item.EMPLOYEE_ID);
                     if (EmployeeBusinessUnitFailures != null)
                     {
                         WarningList.Add(EmployeeBusinessUnitFailures);
-                        X.Js.Call("parent.App.uxTabPostButton.disable(); parent.App.uxPostActivityButton.disable(); parent.App.uxApproveActivityButton.disable(); parent.App.uxTabApproveButton.disable()");
                     }
                     WarningData EmployeeOver24 = ValidationChecks.checkEmployeeTime(24, item.PERSON_ID, (DateTime)item.DA_DATE);
                     if (EmployeeOver24 != null)
@@ -168,7 +168,6 @@ namespace DBI.Web.EMS.Views.Modules.DailyActivity
                     if (BusinessUnitWarning != null)
                     {
                         WarningList.Add(BusinessUnitWarning);
-                        X.Js.Call("parent.App.uxTabPostButton.disable(); parent.App.uxPostActivityButton.disable(); parent.App.uxApproveActivityButton.disable(); parent.App.uxTabApproveButton.disable()");
                     }
                     WarningData MeterWarning = ValidationChecks.MeterCheck(item.EQUIPMENT_ID);
                     if (MeterWarning != null)
