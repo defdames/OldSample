265a58a0cd039e59c8692acc212a29f3b77ef517
Brian Scully <brian.scully@dbiservices.com>
2014-07-22 10:00:25 -0400

    EV-73 Started progess on changing everything over to DBI.Data

---
+++ DBI.Data/Oracle/CROSSING_MAINTENANCE.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBI.Data.Generic;
+
+
+namespace DBI.Data
+{
+    public class CROSSING_MAINTENANCE
+    {
+        //public static List<DoubleComboLongId> BudgetVersions()
+        //{
+        //    using (Entities context = new Entities())
+        //    {
+        //        List<long> OrgsList = SYS_USER_ORGS.GetUserOrgs(SYS_USER_INFORMATION.UserID(User.Identity.Name)).Select(x => x.ORG_ID).ToList();
+        //        long RailRoadId = long.Parse(e.Parameters["RailroadId"]);
+        //        var data = (from v in _context.PROJECTS_V
+        //                    where !(from p in _context.CROSSING_PROJECT
+        //                            where v.PROJECT_ID == p.PROJECT_ID && p.RAILROAD_ID == RailRoadId
+        //                            select p.PROJECT_ID)
+        //                        .Contains(v.PROJECT_ID)
+        //                    where v.PROJECT_TYPE == "CUSTOMER BILLING" && v.TEMPLATE_FLAG == "N" && v.PROJECT_STATUS_CODE == "APPROVED" && v.ORGANIZATION_NAME.Contains(" RR") && OrgsList.Contains(v.CARRYING_OUT_ORGANIZATION_ID)
+        //                    select new { v.PROJECT_ID, v.LONG_NAME, v.ORGANIZATION_NAME, v.SEGMENT1 }).ToList<object>();
+
+
+        //        //uxProjectGrid.Store.Primary.DataSource = data;
+        //        int count;
+        //        uxProjectGrid.Store.Primary.DataSource = GenericData.EnumerableFilterHeader<object>(e.Start, e.Limit, e.Sort, e.Parameters["filterheader"], data, out count);
+        //        e.Total = count;
+        //        ;
+        //    }
+        //}
+    }
+}
