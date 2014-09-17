591e8218ac8f693f8b8a13ddc2563393ab7cd3cc
Damien Dolan <damien.dolan@dbiservices.com>
2014-09-17 13:35:25 -0400

    EV-364 Commit to change branches

--- DBI.Web.EMS/Views/Modules/BudgetBidding/Reports/Report2.cs
+++ DBI.Web.EMS/Views/Modules/BudgetBidding/Reports/Report2.cs
@@ -12,13 +12,18 @@ namespace DBI.Web.EMS.Views.Modules.BudgetBidding.Reports
     /// </summary>
     public partial class Report2 : Telerik.Reporting.Report
     {
-        public Report2()
+        public Report2(string orgName, Int64 orgID,  Int64 yearID,  Int64 verID,  Int64 prevYearID,  Int64 prevVerID)
         {
             //
             // Required for telerik Reporting designer support
             //
             InitializeComponent();
-
+            this.ReportParameters["orgName"].Value = orgName;
+            this.ReportParameters["orgID"].Value = orgID;
+            this.ReportParameters["yearID"].Value = yearID;
+            this.ReportParameters["verID"].Value = verID;
+            this.ReportParameters["prevYearID"].Value = prevYearID;
+            this.ReportParameters["prevVerID"].Value = prevVerID;
             //
             // TODO: Add any constructor code after InitializeComponent call
             //
--- DBI.Web.EMS/Views/Modules/BudgetBidding/Reports/umReport2.aspx.cs
+++ DBI.Web.EMS/Views/Modules/BudgetBidding/Reports/umReport2.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using Telerik.Reporting;
 
 namespace DBI.Web.EMS.Views.Modules.BudgetBidding.Reports
 {
@@ -15,11 +16,15 @@ namespace DBI.Web.EMS.Views.Modules.BudgetBidding.Reports
              //string orgName = Request.QueryString["orgName"];
             //long orgID = Convert.ToInt64(Request.QueryString["orgID"]);
 
-            Report2 report = new Report2();
-            report.ReportParameters["orgName"].Value = Request.QueryString["orgName"];
-            report.ReportParameters["orgID"].Value = Convert.ToInt64(Request.QueryString["orgID"]);
+            
+            Report rareport = new Report2(Request.QueryString["orgName"], Convert.ToInt64(Request.QueryString["orgID"]), Convert.ToInt64(Request.QueryString["yeargID"]),Convert.ToInt64(Request.QueryString["verID"]), Convert.ToInt64(Request.QueryString["prevYearID"]), Convert.ToInt64(Request.QueryString["prevVerID"])) ;
+            //report.ReportParameters["orgID"].Value = Convert.ToInt64(Request.QueryString["orgID"]);
+            //report.ReportParameters["yearID"].Value = Convert.ToInt64(Request.QueryString["yeargID"]);
+            //report.ReportParameters["verID"].Value = Convert.ToInt64(Request.QueryString["verID"]);
+            //report.ReportParameters["prevYearID"].Value = Convert.ToInt64(Request.QueryString["prevYearID"]);
+            //report.ReportParameters["prevVerID"].Value = Convert.ToInt64(Request.QueryString["prevVerID"]);
 
-            ReportViewer1.Report = report;
+            ReportViewer1.Report = rareport;
 
             
 
