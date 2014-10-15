64015e45addc39eb3a0b921f1e93a9519b615b8f
Lenny Jankowski <lenny.jankowski@dbiservices.com>
2014-10-15 17:30:56 -0400

    Updated code for new Fleet module

---
+++ DBI.Web.EMS/Views/Modules/Fleet/umEquipmentThermalPrintReport.aspx.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using DBI.Data;
+using Ext.Net;
+
+namespace DBI.Web.EMS.Views.Modules.Overhead
+{
+    public partial class umEquipmentThermalPrintReport : DBI.Core.Web.BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!X.IsAjaxRequest)
+            {
+                if (!validateComponentSecurity("SYS.Fleet.Reports.TruckLabels"))
+                {
+                    X.Redirect("~/Views/uxDefault.aspx");
+                }
+            }
+        }
+
+        [DirectMethod]
+        public void AddTabPanel(string id, string title, string url)
+        {
+
+            Ext.Net.Panel pan = new Ext.Net.Panel();
+
+            pan.ID = "Tab" + id.Replace(":", "_");
+            pan.Title = title;
+            pan.CloseAction = CloseAction.Destroy;
+            pan.Closable = true;
+            pan.Loader = new ComponentLoader();
+            pan.Loader.ID = "loader" + id.Replace(":", "_");
+            pan.Loader.Url = url;
+            pan.Loader.Mode = LoadMode.Frame;
+            pan.Loader.LoadMask.ShowMask = true;
+            pan.Loader.DisableCaching = true;
+            pan.AddTo(uxCenterTabPanel);
+
+            uxCenterTabPanel.SetActiveTab("Tab" + id.Replace(":", "_"));
+        }
+
+
+        protected void deLoadOrgTree(object sender, NodeLoadEventArgs e)
+        {
+
+            // Add LE
+            if (e.NodeID == "0")
+            {
+                var data = HR.LegalEntityHierarchies().Select(a => new { a.ORGANIZATION_ID, a.ORGANIZATION_NAME }).Distinct().ToList();
+
+
+                //Build the treepanel
+                foreach (var view in data)
+                {
+                    //Only show for the profile option valid 
+
+                    string _profileValue = SYS_ORG_PROFILE_OPTIONS.OrganizationProfileOption("OverheadBudgetHierarchy", view.ORGANIZATION_ID);
+
+                    if (_profileValue.Length > 0)
+                    {
+                        long _profileLong = long.Parse(_profileValue);
+                        var _hierData = HR.LegalEntityHierarchies().Where(a => a.ORGANIZATION_ID == view.ORGANIZATION_ID & a.ORGANIZATION_STRUCTURE_ID == _profileLong).ToList();
+
+                        //Check for incomplete setup 
+                        int _cnt = OVERHEAD_BUDGET_TYPE.BudgetTypes(view.ORGANIZATION_ID).Count();
+
+
+                        //Create the Hierarchy Levels
+                        Node node = new Node();
+                        node.Text = view.ORGANIZATION_NAME;
+                        node.NodeID = string.Format("{0}:{1}", _profileValue.ToString(), view.ORGANIZATION_ID.ToString());
+                        node.Leaf = false;
+                        node.Icon = Icon.Building;
+                        e.Nodes.Add(node);
+                    }
+                }
+
+            }
+
+            // Add orgs
+            else
+            {
+                char[] delimChars = { ':' };
+                string[] selID = e.NodeID.Split(delimChars);
+                long hierarchyID = long.Parse(selID[0].ToString());
+                long orgID = long.Parse(selID[1].ToString());
+                long leID = 0;
+
+                if (selID.Count() == 2)
+                {
+                    leID = orgID;
+                }
+                else
+                {
+                    leID = long.Parse(selID[2].ToString());
+                }
+
+                var data = HR.OverheadOrganizationStatusByHierarchy(hierarchyID, orgID);
+
+                foreach (var view in data)
+                {
+                    if (view.HIER_LEVEL == 1)
+                    {
+                        Node node = new Node();
+                        node.NodeID = string.Format("{0}:{1}:{2}", hierarchyID.ToString(), view.ORGANIZATION_ID.ToString(), leID.ToString());
+                        node.Text = view.ORGANIZATION_NAME;
+
+                        var nextData = HR.ActiveOrganizationsByHierarchy(hierarchyID, view.ORGANIZATION_ID);
+
+
+                        if (nextData.Count() == 0)
+                        {
+                            node.Leaf = true;
+                            node.Icon = Icon.ControlBlank;
+                            e.Nodes.Add(node);
+                        }
+                        else
+                        {
+                            node.Leaf = false;
+                            node.Icon = Icon.ControlBlank;
+                            e.Nodes.Add(node);
+                        }
+                    }
+                }
+            }
+        }
+
+        protected void deSelectNode(object sender, DirectEventArgs e)
+        {
+            TreeSelectionModel sm = uxOrgPanel.GetSelectionModel() as TreeSelectionModel;
+
+            if (sm.SelectedRecordID != "0")
+            {
+                char[] delimChars = { ':' };
+                string[] selID = sm.SelectedRecordID.Split(delimChars);
+                long hierarchyID = long.Parse(selID[0].ToString());
+                long orgID = long.Parse(selID[1].ToString());
+
+                
+               string _nodeText = e.ExtraParams["ORGANIZATION_NAME"];
+               AddTab(sm.SelectedRecordID + "OS", _nodeText + " - Equipment List", "umViewEquipmentList.aspx?orgid=" + sm.SelectedRecordID + "&desc=" + _nodeText + " - Equipment List",true,true);
+
+             }
+        }
+
+        protected void AddTab(string id, string title, string url, Boolean loadContent = false, Boolean setActive = false)
+        {
+
+            Ext.Net.Panel pan = new Ext.Net.Panel();
+
+            pan.ID = "Tab" + id.Replace(":", "_");
+            pan.Title = title;
+            pan.CloseAction = CloseAction.Destroy;
+            pan.Loader = new ComponentLoader();
+            pan.Loader.ID = "loader" + id.Replace(":", "_");
+            pan.Loader.Url = url;
+            pan.Loader.Mode = LoadMode.Frame;
+            pan.Closable = true;
+            pan.Loader.LoadMask.ShowMask = true;
+            pan.Loader.DisableCaching = true;
+            pan.AddTo(uxCenterTabPanel);
+
+            if (setActive)
+                uxCenterTabPanel.SetActiveTab("Tab" + id.Replace(":", "_"));
+
+            if (loadContent)
+                pan.LoadContent();
+        }
+    }
+}
---
+++ DBI.Web.EMS/Views/Modules/Fleet/umViewEquipmentList.aspx.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DBI.Core;
+using DBI.Data;
+using Ext.Net;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using MessagingToolkit.Barcode;
+using MessagingToolkit.Barcode.Common;
+
+namespace DBI.Web.EMS.Views.Modules.Fleet
+{
+    public partial class umViewEquipmentList : DBI.Core.Web.BasePage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void uxOrganizationEquipmentListStore_ReadData(object sender, StoreReadDataEventArgs e)
+        {
+            string _selectedRecordID = Request.QueryString["orgid"];
+
+            char[] _delimiterChars = { ':' };
+            string[] _selectedID = _selectedRecordID.Split(_delimiterChars);
+            long _hierarchyID = long.Parse(_selectedID[0].ToString());
+            long _organizationID = long.Parse(_selectedID[1].ToString());
+
+            //First get a list of organizations
+            List<string> _organizationList = HR.OrganizationsByHierarchy(_hierarchyID, _organizationID).Select(x =>x.ORGANIZATION_ID.ToString()).ToList();
+
+            using (Entities _context = new Entities())
+            {
+                List<PROJECTS_V> _data = (from _dbData in _context.PROJECTS_V
+                                          where (_dbData.PROJECT_STATUS_CODE == "APPROVED" & _dbData.PROJECT_TYPE == "TRUCK & EQUIPMENT")
+                                                select _dbData).ToList();
+
+                List<PROJECTS_V> _returnData = new List<PROJECTS_V>();
+
+                foreach (PROJECTS_V _project in _data)
+                {
+                    if (_organizationList.Contains(_project.CARRYING_OUT_ORGANIZATION_ID.ToString()))
+                        _returnData.Add(_project);
+                }
+
+                int count;
+                uxOrganizationEquipmentListStore.DataSource = GenericData.EnumerableFilterHeader<PROJECTS_V>(e.Start, e.Limit, e.Sort, e.Parameters["filterheader"], _returnData, out count);
+                e.Total = count;
+            }
+        }
+
+        public void dePrintLabels(object sender, DirectEventArgs e)
+        {
+            string _selectedRecordID = Request.QueryString["orgid"];
+            BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+
+            char[] _delimiterChars = { ':' };
+            string[] _selectedID = _selectedRecordID.Split(_delimiterChars);
+            long _hierarchyID = long.Parse(_selectedID[0].ToString());
+            long _organizationID = long.Parse(_selectedID[1].ToString());
+
+            //First get a list of organizations
+            List<string> _organizationList = HR.OrganizationsByHierarchy(_hierarchyID, _organizationID).Select(x => x.ORGANIZATION_ID.ToString()).ToList();
+
+            using (Entities _context = new Entities())
+            {
+                List<PROJECTS_V> _data = (from _dbData in _context.PROJECTS_V
+                                          where (_dbData.PROJECT_STATUS_CODE == "APPROVED" & _dbData.PROJECT_TYPE == "TRUCK & EQUIPMENT")
+                                          select _dbData).ToList();
+
+                List<PROJECTS_V> _returnData = new List<PROJECTS_V>();
+
+                foreach (PROJECTS_V _project in _data)
+                {
+                    if (_organizationList.Contains(_project.CARRYING_OUT_ORGANIZATION_ID.ToString()))
+                    {
+                            _returnData.Add(_project);
+                    }
+                }
+
+                 RowSelectionModel sm = uxEquipmentSelection;
+                 if (sm.SelectedRows.Count() > 0)
+                 {
+                     List<string> _selected = sm.SelectedRows.ToList().Select(x => x.RecordID).ToList();
+                     _returnData = _returnData.Where(x => _selected.Contains(x.PROJECT_ID.ToString())).ToList();
+                 }
+
+                iTextSharp.text.Rectangle pgSize = new iTextSharp.text.Rectangle(144f, 72f);
+                Document ExportedPDF = new Document(pgSize, 0f, 0f, 0f, 0f);
+                string _filename = _organizationID + "_report.pdf";
+                string _filePath = Request.PhysicalApplicationPath + _filename;
+                PdfWriter ExportWriter = PdfWriter.GetInstance(ExportedPDF, new FileStream(_filePath, FileMode.Create));
+                iTextSharp.text.Font cellFont = FontFactory.GetFont("Verdana", 10, iTextSharp.text.Font.BOLD);
+                ExportedPDF.Open();
+
+                PdfPTable _headerPdfTable = new PdfPTable(2);
+                _headerPdfTable.WidthPercentage = 100;
+                int[] intTblWidth = { 75, 25 };
+                _headerPdfTable.SetWidths(intTblWidth);
+                PdfPCell[] Cells;
+                PdfPRow Row;
+
+                foreach (var equip in _returnData)
+                {
+
+                    System.Drawing.Image image = barcodeEncoder.Encode(BarcodeFormat.QRCode, equip.SEGMENT1);
+                    iTextSharp.text.Image QRCodeImage = iTextSharp.text.Image.GetInstance(image, System.Drawing.Imaging.ImageFormat.Png);
+                    QRCodeImage.Alignment = iTextSharp.text.Image.LEFT_ALIGN;
+                    QRCodeImage.ScaleToFit(140f, 70f);
+                    QRCodeImage.SetDpi(300, 300);
+
+                    PdfPCell imageCell = new PdfPCell(QRCodeImage);
+
+                    Phrase _equipmentName = new Phrase();
+                    _equipmentName.Add(new Chunk(equip.NAME, cellFont));
+                    _equipmentName.Add(new Chunk("\n"));
+                    _equipmentName.Add(new Chunk(equip.DESCRIPTION, cellFont));
+
+                    Cells = new PdfPCell[]{
+                     new PdfPCell(imageCell),
+                      new PdfPCell(new Phrase(_equipmentName))
+                    };
+
+                    int cellCount = 0;
+
+                    foreach (PdfPCell _cell in Cells)
+                    {
+                        if (cellCount == 0)
+                        {
+                            _cell.HorizontalAlignment = PdfCell.ALIGN_CENTER;
+                            _cell.Border = iTextSharp.text.Rectangle.NO_BORDER;
+                        }
+                        else
+                        {
+                            _cell.HorizontalAlignment = PdfCell.ALIGN_CENTER;
+                            _cell.Border = iTextSharp.text.Rectangle.NO_BORDER;
+                            _cell.Rotation = 270;
+                        }
+                        cellCount = cellCount + 1;
+                        _cell.UseAscender = true;
+
+                    }
+
+                    Row = new PdfPRow(Cells);
+                    _headerPdfTable.Rows.Add(Row);
+                }
+
+                ExportedPDF.Add(_headerPdfTable);
+                ExportedPDF.Close();
+
+                string baseUrl = Request.Url.GetLeftPart(UriPartial.Authority);
+
+                string _description = Request.QueryString["desc"];
+
+                X.Js.Call("parent.App.direct.AddTabPanel", "p" + _organizationID, _description, "~/" + _filename);
+
+            }
+        }
+    }
+}
