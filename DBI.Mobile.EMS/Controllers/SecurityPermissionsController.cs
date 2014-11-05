8a7acb53f94bb0db8f1fc5e6dedc49d298161874
Lenny Jankowski <lenny.jankowski@dbiservices.com>
2014-11-05 17:28:00 -0500

    Added code for security organizations, works upon login

---
+++ DBI.Mobile.EMS/Controllers/SecurityPermissionsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DBI.Data;
+
+namespace DBI.Mobile.EMS.Controllers
+{
+    public class SecurityPermissionsController : ApiController
+    {
+
+        //[Authorize]
+        //[HttpGet]
+        //public List<long> Get(string username)
+        //{
+        //    //Get UserID using usernamne
+        //    SYS_USER_INFORMATION _userInformation = SYS_USER_INFORMATION.UserByUserName(username);
+
+        //    SYS_USER_PERMS.GetUserPermissions(_userInformation.USER_ID);
+
+        //    return _userOrgs;
+        //}
+
+    }
+}
