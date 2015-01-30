a6562496db1aa27a4e22314cc4118fef93cb1349
Lenny Jankowski <lenny.jankowski@dbiservices.com>
2015-01-30 11:26:38 -0500

    Adjusted javascript to focus on button and then tab into numberfield

--- DBI.Web.EMS/Views/Modules/Overhead/umAddOverheadDetailLine.aspx.designer.cs
+++ DBI.Web.EMS/Views/Modules/Overhead/umAddOverheadDetailLine.aspx.designer.cs
@@ -40,6 +40,15 @@ namespace DBI.Web.EMS.Views.Modules.Overhead {
         protected global::Ext.Net.Viewport Viewport1;
         
         /// <summary>
+        /// uxCalculatorEdit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::Ext.Net.Button uxCalculatorEdit;
+        
+        /// <summary>
         /// GridPanel3 control.
         /// </summary>
         /// <remarks>
